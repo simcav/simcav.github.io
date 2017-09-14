9997b104047f2d03ff001d1e670fdbe9b5048e09
jrphysics <dr.rodriguez-garcia@protonmail.com>
2017-09-14 09:12:17 +0100

    Update mainjs.js

--- mainjs.js
+++ mainjs.js
@@ -1,7 +1,7 @@
 function showdiv(div1) {
   var divs = ["divhome", "divdown", "divman", "divcon"]
   for (i = 0; i < divs.length; i++) {
-    document.getElementById(divs(i)).className = "hidden";
+    document.getElementById(divs[i]).className = "hidden";
   }
   document.getElementById(divs1).className = "nothidden";
 }

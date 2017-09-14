d21ecae0e62300e73be0085ae92118920f719590
jrphysics <dr.rodriguez-garcia@protonmail.com>
2017-09-14 09:28:35 +0100

    Update mainjs.js

--- mainjs.js
+++ mainjs.js
@@ -1,4 +1,4 @@
-window.onload = function showdiv(div1) {
+function showdiv(div1) {
   var divs = ["divhome", "divdown", "divman", "divcon"]
   for (i = 0; i < divs.length; i++) {
     document.getElementById(divs[i]).className = "hidden";

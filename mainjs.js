8126b2d470dbfbc8bdc87fd3474831962a43930c
jrphysics <dr.rodriguez-garcia@protonmail.com>
2017-11-21 20:00:37 +0000

    Change in z-index of overlapping divs

--- mainjs.js
+++ mainjs.js
@@ -2,8 +2,10 @@ function showdiv(div1) {
   var divs = ["divhome", "divdown", "divcon"]
   for (i = 0; i < divs.length; i++) {
     document.getElementById(divs[i]).className = "hidden";
+    document.getElementById(id).style.zIndex=0;
   }
   document.getElementById(div1).className = "nothidden";
+  document.getElementById(id).style.zIndex=1;
 }
 
 function showemail() {

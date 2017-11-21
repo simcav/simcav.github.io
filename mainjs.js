a81d17c17cf5e597583da3d4fdda5fd5f4c9c85b
jrphysics <dr.rodriguez-garcia@protonmail.com>
2017-11-21 20:09:10 +0000

    Remove z-index from here
    z-index will be controlled by css

--- mainjs.js
+++ mainjs.js
@@ -2,10 +2,8 @@ function showdiv(div1) {
   var divs = ["divhome", "divdown", "divcon"]
   for (i = 0; i < divs.length; i++) {
     document.getElementById(divs[i]).className = "hidden";
-    document.getElementById(divs[i]).style.zIndex=10;
   }
   document.getElementById(div1).className = "nothidden";
-  document.getElementById(divs[i]).style.zIndex=11;
 }
 
 function showemail() {

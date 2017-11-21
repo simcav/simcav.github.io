c977c0ffc2cae74e9cab2ea1a072426c5a1222e4
jrphysics <dr.rodriguez-garcia@protonmail.com>
2017-11-21 20:04:05 +0000

    z-index fix 2

--- mainjs.js
+++ mainjs.js
@@ -2,10 +2,10 @@ function showdiv(div1) {
   var divs = ["divhome", "divdown", "divcon"]
   for (i = 0; i < divs.length; i++) {
     document.getElementById(divs[i]).className = "hidden";
-    document.getElementById(id).style.zIndex=10;
+    document.getElementById(divs[i]).style.zIndex=10;
   }
   document.getElementById(div1).className = "nothidden";
-  document.getElementById(id).style.zIndex=11;
+  document.getElementById(divs[i]).style.zIndex=11;
 }
 
 function showemail() {
